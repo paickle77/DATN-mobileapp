bfbeb65cd736b45f08c0a6b96c3fee31fda6cdc6
hocbtph48716 <hocbuj2001@gmail.com>
2025-06-25 22:58:08 +0700

    update api

--- app/(tabs)/services/api.ts
+++ app/(tabs)/services/api.ts
@@ -1,6 +1,2 @@
-<<<<<<<<< Temporary merge branch 1
-export const BASE_URL = 'http://192.168.190.104:3000/api';
-=========
-export const BASE_URL = 'http://172.20.20.7:3000/api';
->>>>>>>>> Temporary merge branch 2
+export const BASE_URL = 'http://192.168.0.110:3000/api';
 
